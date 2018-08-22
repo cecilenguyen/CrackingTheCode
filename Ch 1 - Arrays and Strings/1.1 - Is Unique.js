2a02a3db727b914646d8a135f591eab7d1655187
Cecile Nguyen <uyen.nguyen36@yahoo.com>
2018-08-21 18:19:14 -0700

    1.1

---
+++ Ch 1 - Arrays and Strings/1.1 - Is Unique.js
@@ -0,0 +1,8 @@
+/* 1.1 Is Unique: Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures? */
+
+
+// with additional data structure
+function isUnique(str) {
+	let seenChars = [];
+	for (let i = 0; i < )
+}
